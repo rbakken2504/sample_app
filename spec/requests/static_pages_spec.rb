035ecf84f63aeaf1c3129e9113ea319f2b4b60ad
= <=>
2014-06-30 00:08:37 -0500

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,10 +5,25 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title("#{base_title} | About Us")
+    click_link "Help"
+    expect(page).to have_title("#{base_title} | Help")
+    click_link "Contact"
+    expect(page).to have_title("#{base_title} | Contact Us")
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title("#{base_title} | Sign Up")
+    click_link "sample app"
+    expect(page).to have_title("#{base_title}")
+  end
+
   describe "Home page" do
     before { visit root_path }
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title("#{base_title}") }
     it { should_not have_title('| Home') }
   end
 
@@ -26,7 +41,7 @@ describe "StaticPages" do
 
   describe "Contact Page" do
     before { visit contact_path }
-    it { should have_content('Contact Us') }
+    it { should have_selector('h1', text: 'Contact Us') }
     it { should have_title("#{base_title} | Contact Us") }
   end
 end
